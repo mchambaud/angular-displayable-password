dfc3b31d2c56cbde48e6d6a19445cc918b47084c
Michael Chambaud <mchambaud@mixgenius.com>
2015-03-09 10:20:34 -0400

    first commit

---
+++ angular-displayable-password.js
@@ -0,0 +1,40 @@
+'use strict';
+
+angular.module('mchambaud.angular-displayable-password', [])
+	.directive('displayablePassword', displayablePassword);
+
+function displayablePassword() {
+	return {
+		restrict: 'E',
+		replace: true,
+		scope: {
+			password:'=ngModel',
+			placeholder:'@',
+			required:'=ngRequired',
+			minLength: '=ngMinlength',
+			maxLength: '=ngMaxlength',
+			pattern: '=ngPattern'
+		},
+		template:	'<div class="angular-displayable-password">' +
+						'<input type="text"' +
+							'placeholder="{{placeholder}}"' +
+							'ng-hide="isVisible"' +
+							'ng-model="password"> ' +
+
+						'<input type="password"' +
+							'placeholder="{{placeholder}}"' +
+							'ng-show="isVisible"' +
+							'ng-model="password"' +
+							'ng-required="required"' +
+							'ng-maxlength="maxLength"' +
+							'ng-minlength="minLength"' +
+							'ng-pattern="pattern"> ' +
+
+						'<input type="checkbox" ' +
+							'ng-click="isVisible = !isVisible"> ' +
+					'</div>',
+		link: function(scope, element, attrs){
+			scope.isVisible = true;
+		}
+	}
+}
